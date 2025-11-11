a0bce16a77713751b894b7c8ee677699ab0d6e1d
Nitish <nitishkumar1@districtd.co>
2025-11-11 15:38:06 +0530

    Add EditorConfig for consistent formatting

--- src/database/database.connection.ts
+++ src/database/database.connection.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Sequelize } from 'sequelize-typescript';
 import { Dialect } from 'sequelize';
 import * as db from '../models';
